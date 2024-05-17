7938c8c7d75063fa97566edd6459ae421360c99e
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-05-17 17:35:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ useTexture.preload('https://assets.vercel.com/image/upload/contentful/image/e538
 export default function App() {
   const { debug } = useControls({ debug: false })
   return (
-    <Canvas camera={{ position: [0, 0, 8], fov: 25 }}>
+    <Canvas camera={{ position: [0, -2, 8], fov: 25 }}>
       <ambientLight intensity={Math.PI} />
       <Physics debug={debug} interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
         <Band />
@@ -117,19 +117,19 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
 
 
             <Center bottom right>
-              <Resize key={4} maxHeight={0.45} maxWidth={0.925}>
+               
                 <Text3D
                   bevelEnabled={false}
                   bevelSize={0}
                   font="asd.json"
-                  scale={[1/8 , 1/8 , 1/8 ]}
+                  scale={[1/12 , 1/12 , 1/12 ]}
                   height={0}
-                  position={[9, 42, 1]}
+                  position={[-0.96, 1.4, 1.1]}
                   rotation={[0, 0, 0]}
                 >
                   {`JULIALAB\n  code\nsandbox`}
                 </Text3D>
-              </Resize>
+               
             </Center>
 
 
